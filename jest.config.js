1b38aa3610bbdcf8f3c2bd911ba099e79a162bed
nikitonskii <panaskin.1001@gmail.com>
2026-08-04 23:32:54 +0200

    test(core): purity scan and 100% coverage gate on src/core (AC-13)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  collectCoverageFrom: ['src/core/**/*.ts', '!src/core/**/index.ts', '!src/core/**/types.ts'],
+  coverageThreshold: {
+    './src/core/': {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 };
